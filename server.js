ab63600e7f4e666dd7d82a7aed729179851715ea
hazardvx <litlehazard@gmail.com>
2026-09-26 13:49:54 -0500

    ITBMS en facturas (0/7/10/15), descuento fantasma corregido, import/export con validacion y sesion estable, buscadores en listas, abonos validados, papel con ITBMS; server.js + package.json para despliegue (Seenode)

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+const ROOT = __dirname;
+const TYPES = {
+  ".html": "text/html; charset=utf-8",
+  ".js": "text/javascript; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".json": "application/json; charset=utf-8",
+  ".png": "image/png",
+  ".svg": "image/svg+xml",
+  ".ico": "image/x-icon",
+  ".webmanifest": "application/manifest+json"
+};
+
+http.createServer((req, res) => {
+  let p = decodeURIComponent((req.url || "/").split("?")[0]);
+  if (p === "/") p = "/index.html";
+  const file = path.normalize(path.join(ROOT, p));
+  if (!file.startsWith(ROOT)) {
+    res.writeHead(403);
+    return res.end("Forbidden");
+  }
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
+      return res.end("<h1>404</h1>");
+    }
+    res.writeHead(200, { "Content-Type": TYPES[path.extname(file)] || "application/octet-stream" });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log("JTAS Hydraulic Hose escuchando en puerto " + PORT));
